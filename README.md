0e8beaaaa0456308e013b71e5c68a3c5600c33f9
wj107 <will.johnson@gmail.com>
2019-04-18 23:52:35 -0500

    script to dl and clean Com Ed data

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 ### ComEd
 
-Average hourly load -- energy usage, hour-by-hour -- data from ComEd.  Available from April 2009.
+Average hourly load -- energy usage, hour-by-hour -- data from ComEd.  Available from April 2009.  Run `get_data.R` script, and it will donwload data as an xls file, convert into 5 different data frames, containing average hourly electricity usage for:
+
+- Single family houses
+- Multi family houses
+- Small businesses
+- Medium businesses
+- Streetlights
+
+Not sure the specific differences between those categories; documentation is scant.  If you want more info, probably best to start looking at the [Com Ed website](https://www.comed.com/MyAccount/MyService/Pages/ARCHIVE/HistoricalLoadProfiles.aspx).
--- get_data.R
+++ get_data.R
@@ -11,4 +11,28 @@ download.file(link,destfile="comed_data.xls")
 ##---re-write with readxl package/tidyverse!!!
 require(gdata)
 
-read.xls("comed_data.xls")->dat
+read.xls("comed_data.xls",sheet=1)->single.family.elec
+read.xls("comed_data.xls",sheet=2)->multi.family.elec
+read.xls("comed_data.xls",sheet=6)->small.bus.elec
+read.xls("comed_data.xls",sheet=7)->med.bus.elec
+read.xls("comed_data.xls",sheet=8)->streetlight.elec
+
+#----name data
+Names<-c("date",paste0(1:24,"h"))
+names(single.family.elec)<-Names
+names(multi.family.elec)<-Names
+names(small.bus.elec)<-Names
+names(med.bus.elec)<-Names
+names(streetlight.elec)<-Names
+
+#---trim extra columns
+single.family.elec<-single.family.elec[,1:25]
+multi.family.elec<-multi.family.elec[,1:25]
+small.bus.elec<-small.bus.elec[,1:25]
+med.bus.elec<-med.bus.elec[,1:25]
+streetlight.elec<-streetlight.elec[,1:25]
+
+#---delete large xls file
+file.remove("comed_data.xls")
+
+
